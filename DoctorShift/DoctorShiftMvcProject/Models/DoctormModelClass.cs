823cf9bcae7cf27626ec76f16e0950c350700824
gltkn050 <gltkn050@gmail.com>
2023-07-07 18:05:49 +0300

    Layout oluşturuldu. Versiyon2 07.07.2023

---
+++ DoctorShift/DoctorShiftMvcProject/Models/DoctormModelClass.cs
@@ -0,0 +1,37 @@
+﻿using static DoctorShiftMvcProject.Models.DoctormModelClass;
+
+namespace DoctorShiftMvcProject.Models
+{
+    public class DoctormModelClass
+    {
+        // This class defining the doctor profiles. 
+        public string? name { get; set; }
+        public string? surname { get; set; }
+        public Guid? id { get; set; }
+        public enum Rank
+        {
+            None,
+            ExaminationDoctor,
+            ResuscitationDoctor,
+            SurgialOperationDoctor,
+            SeniorDoctor,
+        }
+        public int shiftNumber { get; set; }
+        public int phoneNumber { get; set; }
+        public bool workingState { get; set; }
+        public Rank doctorRank { get; set; }
+
+        //Constructor metod :)
+        public DoctormModelClass(string Name, string Surname, Guid Id, Rank DoctorRank, int ShiftNumber, int PhoneNumber, bool WorkingState)
+        {
+            name = Name;
+            surname = Surname;
+            id = Id;
+            shiftNumber = ShiftNumber;
+            phoneNumber = PhoneNumber;
+            workingState = WorkingState;
+            doctorRank = DoctorRank;
+
+        }
+    }
+}
---
+++ DoctorShift/DoctorShiftMvcProject/Models/ShiftClass.cs
@@ -0,0 +1,43 @@
+﻿namespace DoctorShiftMvcProject.Models
+{
+    public class ShiftClass
+    {
+        public bool nightShift { get; set; }
+        public bool morningShift { get; set; }
+        public bool weekend { get; set; }
+        public bool weekday { get; set; }
+        public enum days
+        {
+            Monday,
+            Tuesday,
+            Wensday,
+            Thirsday,
+            Friday,
+            Saturday,
+            Sunday,
+        }
+        public ShiftClass(bool MorningShift, bool Weekday, days Days)
+        {
+            morningShift = MorningShift;
+            weekday = Weekday;
+            if (morningShift)
+            {
+                nightShift = false;
+            }
+            else
+            {
+                nightShift = true;
+            }
+            if (weekday)
+            {
+                weekend = false;
+            }
+            else
+            {
+                weekend = true;
+            }
+
+
+        }
+    }
+}
